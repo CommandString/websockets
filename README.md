00bd2049ebd4ac3109d78f8e61609a5641df2406
CommandString <bmxmikey2@gmail.com>
2022-12-16 01:01:14 -0500

    Message class

---
+++ README.md
@@ -0,0 +1,14 @@
+# commandstring/websockets #
+A composer package for connecting JS to PHP
+
+# NOTE: THIS IS STILL A WORK IN PROGRESS #
+
+# Setup
+
+`git clone https://github.com/CommandString/websockets`
+`composer install`
+
+# Using the example #
+`php index.php`
+*open another terminal*
+run `php -S localhost:8000 -t ./` in the example directory
---
+++ src/Message.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace CommandString\WebSockets;
+
+use CommandString\WebSockets\Connection\Client;
+use stdClass;
+
+class Message {
+    private stdClass $response;
+    private array $errors = [];
+
+    public function __construct(private Client $client, private stdClass|string $request)
+    {
+        $this->response = new stdClass;
+    }
+
+    public static function new(Client $client, stdClass|string $request): self
+    {
+        return new self($client, $request);
+    }
+
+    public function setResponseData(string $key, mixed $value): self
+    {
+        $this->response->$key = $value;
+        return $this;
+    }
+
+    public function getRequest(): stdClass
+    {
+        return $this->request;
+    }
+
+    public function addError(string $message): self
+    {
+        $this->errors[] = $message;
+        return $this;
+    }
+
+    public function __toString(): string
+    {
+        return json_encode([
+            "response" => $this->response,
+            "request" => $this->request,
+            "errors" => $this->errors
+        ]);
+    }
+
+    public function respond() {
+        $this->client->connection->send($this);
+    }
+}

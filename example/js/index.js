fde719a8986980da9ba6505f7e647a7981ac09bd
CommandString <bmxmikey2@gmail.com>
2022-12-16 01:00:18 -0500

    Example JS

---
+++ example/js/index.js
@@ -0,0 +1,98 @@
+import { Req } from "./Req.js"
+import { Res } from "./Res.js"
+import { Ws } from "./Ws.js"
+
+$.toast({
+    "title": "Attempting to connect to websocket...",
+    "class": "yellow"
+})
+
+let ws = null
+let attempts = 1
+let connected = false
+
+function connect() {
+    ws = new Ws(new WebSocket("ws://localhost:8080"))
+
+    ws.websocket.onopen = () => {
+        $.toast({
+            "title": "Connected to websocket",
+            "class": "green"
+        })
+
+        $("body > .segment").removeClass("loading")
+
+        attempts = 1
+        connected = true
+    }
+
+    ws.websocket.onmessage = (event) => {
+        let res = new Res(event.data)
+
+        if (typeof res.request.requestId != "undefined") {
+            ws.handleRequest(res)
+        }
+    }
+
+    ws.websocket.onclose = (event) => {
+        attempts++
+        let delayIncrement = 500
+        let maxDelay = 5000
+        let delay = attempts * delayIncrement
+
+        if (delay > maxDelay) {
+            delay = maxDelay
+        }
+
+        let title = (connected) ? "Websocket Connection Closed" : "Failed To Connect To Websocket"
+
+        $.toast({
+            "title": title,
+            "message" : `Attempting to reconnect in ${delay} milliseconds`,
+            "class": "red",
+            "displayTime": delay
+        })
+
+        $("body > .segment").addClass("loading")
+
+        setTimeout(() => {
+            connect()
+        }, delay)
+
+        connected = false
+    }
+}
+
+connect()
+
+// USED FOR EXAMPLE PAGE
+$("#addField").click(() => {
+    let fields = $(`[template='data-fields']`).clone()
+
+    fields.removeAttr("template")
+    fields.appendTo("#data-fields")
+})
+
+$("#addField").click()
+
+$("#sendRequest").click(() => {
+    ws.newRequest($(`[name='endpoint']`).val())
+
+    let key = ""
+    $("#data-fields .field input").each(function( i ) {
+        if (i % 2 == 0) {
+            key = $(this).val()
+        } else {
+            ws.setData(key, $(this).val())
+            key = ""
+        }
+
+        if (i == $("#data-fields .field input").length - 1) {
+            ws.send(res => {
+                console.log(res)
+            }, res => {
+                console.error(res)
+            })
+        }
+    })
+})
